c73376707d62681b05206218f31cab5b2a3fb140
James Carlson <jxxcarlson@mac.com>
2018-04-30 11:19:37 -0400

    Add working with text files

---
+++ jupyter/lessons/stat.py
@@ -0,0 +1,17 @@
+import math
+
+def mean(data):
+    return round(sum(data)/float(len(data)), 2)
+
+def delta(data, center):
+    return map(lambda x: x - center, data)
+
+def deviation(data):
+    return delta(data, mean(data))
+
+def variance(data):
+    squares = map(lambda x: x*x, deviation(data))
+    return sum(squares)/len(data)
+
+def stdev(data):
+    return round(math.sqrt(variance(data)),2)
