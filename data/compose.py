c7faf5b2eba99e324eb3cda625928f07aaa23b59
James Carlson <jxxcarlson@mac.com>
2018-05-21 13:57:40 -0400

    Reorganize data analysis files

--- data/compose.py
+++
@@ -1,5 +0,0 @@
-
-import functools
-
-def compose(*functions):
-    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
---
+++ data/programs/plot_data.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+import sys
+from get_data import *
+
+data = get_data(sys.argv[1])
+
+years = first(data)
+anomalies = to_float(second(data))
+
+plt.plot(years, anomalies, color='red', marker='', linestyle='solid')
+plt.title("Temperature anomaly")
+plt.ylabel("Degrees C")
+plt.savefig("anomaly.png")
+plt.show()
